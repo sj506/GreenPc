66d7c1abb2a9b4d2b9256ac244806efefc9fca7d
Seung-Jae <tmdwo5040@gmail.com>
2022-06-26 21:56:33 +0900

    .

---
+++ vite/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <h1>잘하고있다!</h1>
+        </div>
+    );
+}
+
+export default App;
