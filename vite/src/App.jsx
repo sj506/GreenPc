4ae51bac481d72a78523d76543e057a6d429bb20
Seung-Jae <tmdwo5040@gmail.com>
2022-07-01 20:40:40 +0900

    CreateNewArray

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,31 +1,31 @@
 import { useState, useEffect } from 'react';
 
-function Hello() {
-    // function byFn() {
-    //     console.log('bye : (');
-    // }
-    // function hiFn() {
-    //     console.log('hi :)');
-    //     return byFn; // return 값으로 함수를 줄 수도 있음
-    // }
-
-    useEffect(() => {
-        console.log('hi :)');
-        return () => {
-            console.log('by :(');
-        };
-    }, []);
-
-    return <h1>Hello</h1>;
-}
-
 function App() {
-    const [showing, setShowing] = useState(false);
-    const onClick = () => setShowing((prev) => !prev);
+    const [toDo, setToDo] = useState('');
+    const [toDos, setToDos] = useState([]);
+    const onChange = (e) => setToDo(e.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo == '') {
+            return;
+        }
+
+        setToDos((currentArray) => [toDo, ...currentArray]); // [Newtext , 값 넣을 배열]
+        setToDo('');
+    };
+    console.log(toDos);
+
     return (
         <div>
-            {showing ? <Hello /> : null}
-            <button onClick={onClick}>{!showing ? 'show' : 'hide'}</button>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    value={toDo}
+                    type="text"
+                    placeholder="Write your to do..."
+                />
+                <button>Add To Do</button>
+            </form>
         </div>
     );
 }
