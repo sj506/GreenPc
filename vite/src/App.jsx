1fbf6b559ea30d12a57e03960f782c361ecb0a7c
Seung-jae <tmdwo5040@gmail.com>
2022-07-07 12:49:16 +0900

    movies

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,36 +1,36 @@
 import { useState, useEffect } from 'react';
 
 function App() {
-    const [loading, setLoaing] = useState(true);
-    const [coin, setCoin] = useState([]);
-
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
+        const json = await (
+            await fetch(
+                `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101`
+            )
+        ).json();
+        console.log(json.boxOfficeResult.dailyBoxOfficeList);
+        setMovies(json.boxOfficeResult.dailyBoxOfficeList);
+        setLoading(false);
+    };
     useEffect(() => {
-        fetch('https://api.coinpaprika.com/v1/tickers')
-            .then((res) => res.json())
-            .then((res) => {
-                setCoin(res);
-                setLoaing(false);
-            });
+        getMovies();
     }, []);
 
     return (
         <div>
-            <h1>My coins ({loading ? '0' : coin.length})</h1>
-            <input type="number" placeholder="Write Your USD" />
-            <br />
-            {loading ? (
-                <strong>Loading... </strong>
-            ) : (
-                <select>
-                    {coin.map((item) => (
-                        <option>
-                            {item.name}:{item.quotes.USD.price}
-                        </option>
-                    ))}
-                </select>
-            )}
+            <ul>{loading ? <h1>Loading...</h1> : <Ctnt movies={movies} />}</ul>
         </div>
     );
 }
 
+function Ctnt(props) {
+    return props.movies.map((movies) => (
+        <div key={movies.rnum}>
+            <li>
+                {movies.rank}. {movies.movieNm}
+            </li>
+        </div>
+    ));
+}
 export default App;
