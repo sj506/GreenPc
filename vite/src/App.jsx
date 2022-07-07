e0e8bb46185face18c8a183eefe326bb06e3e1fe
Seung-jae <tmdwo5040@gmail.com>
2022-07-07 17:48:38 +0900

    7.7 Movies

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,36 +1,20 @@
-import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async () => {
-        const json = await (
-            await fetch(
-                `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20120101`
-            )
-        ).json();
-        console.log(json.boxOfficeResult.dailyBoxOfficeList);
-        setMovies(json.boxOfficeResult.dailyBoxOfficeList);
-        setLoading(false);
-    };
-    useEffect(() => {
-        getMovies();
-    }, []);
-
     return (
-        <div>
-            <ul>{loading ? <h1>Loading...</h1> : <Ctnt movies={movies} />}</ul>
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/movie">
+                    <Detail />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
-function Ctnt(props) {
-    return props.movies.map((movies) => (
-        <div key={movies.rnum}>
-            <li>
-                {movies.rank}. {movies.movieNm}
-            </li>
-        </div>
-    ));
-}
 export default App;
