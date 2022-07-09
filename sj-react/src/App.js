4be95c87b2e1958b81b935eacf50a2c93be77e33
Seung-Jae <tmdwo5040@gmail.com>
2022-07-09 18:07:06 +0900

    7.9 Link

---
+++ sj-react/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Detail from './routes/Detail';
+import Home from './routes/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
