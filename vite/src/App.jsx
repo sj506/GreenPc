36e45c0ddead56b29fcb5beee68b6c907c4f0805
Seung-Jae <tmdwo5040@gmail.com>
2022-07-09 14:32:54 +0900

    .

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,9 +1,20 @@
-import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  return;
-  <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
